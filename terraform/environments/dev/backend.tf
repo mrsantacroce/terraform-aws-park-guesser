6ef7bdc247ff2352f4fe27696a96d75533fc801a
mrsantacroce <github@mrsantacroce.com>
2025-11-06 02:28:41 -0700

    go back to dynamodb table lock

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-381492209450-us-east-1"
-    key          = "park-guesser/dev/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "terraform-state-381492209450-us-east-1"
+    key            = "park-guesser/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
   }
 }
